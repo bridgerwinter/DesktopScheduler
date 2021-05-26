978263ede66b820f8e45b7bc2ec7271f09258baa
Bridger Winter <bridgerwinter@gmail.com>
2021-05-25 17:32:44 -0700

    Add project files.

---
+++ desktopScheduler/Database.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desktopScheduler
+{
+    class Database
+    {
+        static MySqlConnection con;
+        public static void connectAndOpenDB()
+        {
+            con = new MySqlConnection("server = wgudb.ucertify.com; user id = U057MV; persistsecurityinfo = True; database = U057MV");
+            con.Open();
+        }
+    }
+}
